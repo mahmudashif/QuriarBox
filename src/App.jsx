53e3c45b437ba5fc0f6e8bc64e7858e417ae61e0
ashifmahmud <mdamahmud2000@gmail.com>
2025-03-09 05:51:50 +0600

    feat: Component section create

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Footer from "./Components/layout/Footer";
+import Navbar from "./Components/layout/Navbar";
 
 const App = () => {
-  return <div className=""></div>;
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
