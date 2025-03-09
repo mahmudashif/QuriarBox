1c9e28601816d0f9fdd187c1b1155169ad99378c
ashifmahmud <mdamahmud2000@gmail.com>
2025-03-09 16:43:08 +0600

    feat: services part added

--- src/Components/section/Services.jsx
+++ src/Components/section/Services.jsx
@@ -4,6 +4,7 @@ const Services = () => {
   return (
     <div className="bg-BackgroundColor pt-16">
       <div className="max-w-max-width mx-auto">
+        {/* ---------- Service heading ---------- */}
         <div className="text-center">
           <h2 className="text-BasicOrange font-Oxanium font-bold text-3xl uppercase">
             services
