b944ba954a18f79a5bcb64dde4b6665ce0b65fc1
ashifmahmud <mdamahmud2000@gmail.com>
2025-03-09 06:48:05 +0600

    Fix: nav item menu fixed

--- src/Components/layout/Navbar.jsx
+++ src/Components/layout/Navbar.jsx
@@ -16,27 +16,31 @@ const Navbar = () => {
     <div className="max-w-max-width mx-auto py-5">
       <div className="flex items-center">
         {/* ---------- Logo -------- */}
-        <div className="w-2/4">
+        <div className="w-1/2">
           <img src={Logo} alt="Logo" />
         </div>
         {/* ---------- NavItem -------- */}
-        <div className="w-1/4 flex gap-x-10 font-Raleway">
-          {navItems.map((item, index) => (
-            <ul key={index}>
-              <li className="hover:text-red-500 cursor-pointer">{item.name}</li>
-            </ul>
-          ))}
-        </div>
-        {/* ---------- Button -------- */}
-        <div className="w-1/4 ml-10 flex items-center">
-          {/* ---------- Search Button -------- */}
-          <button className="bg-[#ffedc9] p-3 rounded-xl">
-            <img src={SearchIcon} alt="" />
-          </button>
-          {/* ---------- Contact Us Button -------- */}
-          <button className="bg-[#ffe4d9] py-3 px-5 ml-4 font-Raleway font-bold text-red-700 rounded-xl">
-            Contact us
-          </button>
+        <div className="w-1/2 flex items-center">
+          <div className="flex gap-x-10 font-Raleway">
+            {navItems.map((item, index) => (
+              <ul key={index}>
+                <li className="hover:text-red-500 cursor-pointer font-semibold">
+                  {item.name}
+                </li>
+              </ul>
+            ))}
+          </div>
+          {/* ---------- Button -------- */}
+          <div className="ml-10 flex items-center">
+            {/* ---------- Search Button -------- */}
+            <button className="bg-[#ffedc9] p-3 rounded-xl">
+              <img src={SearchIcon} alt="" />
+            </button>
+            {/* ---------- Contact Us Button -------- */}
+            <button className="bg-[#ffe4d9] py-3 px-5 ml-4 font-Raleway font-bold text-red-700 rounded-xl">
+              Contact us
+            </button>
+          </div>
         </div>
       </div>
     </div>
