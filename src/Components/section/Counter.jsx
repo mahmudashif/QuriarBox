d8f46253d6817c8e1fa6b30b375cbce138f8809c
ashifmahmud <mdamahmud2000@gmail.com>
2025-03-12 21:53:41 +0600

    Your commit message here

--- src/Components/section/Counter.jsx
+++ src/Components/section/Counter.jsx
@@ -39,6 +39,42 @@ const Counter = () => {
             States covered
           </p>
         </div>
+        {/* ---------- State cover counter ---------- */}
+        <div className="w-[15%] text-center">
+          <div className="flex justify-center">
+            <img src={statesCovered} alt="statesCovered" />
+          </div>
+          <p className="text-BasicOrange font-bold text-4xl font-Raleway mt-2">
+            {count}+
+          </p>
+          <p className="text-BasicGray text-xl font-semibold font-Raleway mt-2">
+            States covered
+          </p>
+        </div>
+        {/* ---------- State cover counter ---------- */}
+        <div className="w-[15%] text-center">
+          <div className="flex justify-center">
+            <img src={statesCovered} alt="statesCovered" />
+          </div>
+          <p className="text-BasicOrange font-bold text-4xl font-Raleway mt-2">
+            {count}+
+          </p>
+          <p className="text-BasicGray text-xl font-semibold font-Raleway mt-2">
+            States covered
+          </p>
+        </div>
+        {/* ---------- State cover counter ---------- */}
+        <div className="w-[15%] text-center">
+          <div className="flex justify-center">
+            <img src={statesCovered} alt="statesCovered" />
+          </div>
+          <p className="text-BasicOrange font-bold text-4xl font-Raleway mt-2">
+            {count}+
+          </p>
+          <p className="text-BasicGray text-xl font-semibold font-Raleway mt-2">
+            States covered
+          </p>
+        </div>
       </div>
     </div>
   );
