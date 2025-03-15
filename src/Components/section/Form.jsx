e1024595107d990ed69a8142a1c1faf5b31af312
ashifmahmud <mdamahmud2000@gmail.com>
2025-03-16 00:00:18 +0600

    feat: form part create

--- src/Components/section/Form.jsx
+++ src/Components/section/Form.jsx
@@ -1,15 +1,33 @@
 import React from "react";
+import ladisIcon from "/public/Illustration.svg";
 
 const Form = () => {
   return (
     <div className="bg-BackgroundColor pt-10 pb-10">
-      <div className="max-w-max-width mx-auto">
-        <div>
-          <div>
-            <img src="/public/Illustration.svg" alt="" />
+      <div className="max-w-max-width mx-auto flex items-center justify-between">
+        <div className="w-1/2">
+          <div className="flex justify-center mb-5">
+            <img src={ladisIcon} alt="" />
+          </div>
+          <div className="ml-10">
+            <h3 className="text-BasicOrange font-Oxanium font-bold text-2xl">
+              REQUEST A CALLBACK
+            </h3>
+            <h2 className="font-extrabold text-[40px] text-[#222132] font-Raleway w-[452px] mt-5 leading-10">
+              We will contact in the shortest time.
+            </h2>
+            <p className="text-2xl font-semibold text-[#9291A1] mt-5 font-Raleway">
+              Monday to Friday, 9am-5pm.
+            </p>
           </div>
         </div>
-        <div></div>
+        <div className="w-1/2">
+          <input
+            type="text"
+            placeholder="Name"
+            className="bg-transparent border-[2px] border-[#C5C5D2] rounded-md"
+          />
+        </div>
       </div>
     </div>
   );
