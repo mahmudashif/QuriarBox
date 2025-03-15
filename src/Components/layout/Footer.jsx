c8e47f06fb4657363ec9c4e12d2eb240acde1c21
ashifmahmud <mdamahmud2000@gmail.com>
2025-03-16 04:09:57 +0600

    responsive: footer

--- src/Components/layout/Footer.jsx
+++ src/Components/layout/Footer.jsx
@@ -4,33 +4,37 @@ import logo from "/public/TypographyLogo.svg";
 const Footer = () => {
   return (
     <div className="bg-footerPrimary pt-20 pb-10">
-      <div className="max-w-max-width mx-auto flex justify-between items-center">
-        <div className="text-white">
+      {/* ---------- Footer first layer ---------- */}
+      <div className="max-w-max-width mx-auto flex flex-col lg:flex-row justify-between items-center px-4">
+        <div className="text-white mb-6 lg:mb-0">
           <h4 className="font-Raleway font-bold text-4xl pb-5">
             Get an update every week
           </h4>
-          <p className="font-Raleway text-[16px] w-[500px]">
+          <p className="font-Raleway text-[16px] w-full lg:w-[500px]">
             We ensure that your product is delivered in the safest possible
             manner, at the correct location, at the right time.
           </p>
         </div>
-        <div>
+        <div className="w-full lg:w-auto">
           <p className="font-bold font-Oxanium text-lg text-BasicOrange">
             SUBSCRIBE TO NEWSLETTER
           </p>
-          <input
-            type="email"
-            placeholder="Enter your mail"
-            className="flex-1 bg-transparent border border-gray-500 rounded-md px-4 py-4 text-white outline-none w-[386px] mt-5"
-          />
-          <button className="bg-[#F26522] text-white font-bold px-6 py-4 rounded-md ml-2 font-Oxanium">
-            Subscribe
-          </button>
+          <div className="flex flex-col lg:flex-row mt-5">
+            <input
+              type="email"
+              placeholder="Enter your mail"
+              className="bg-transparent border border-gray-500 rounded-md px-4 py-4 text-white outline-none w-full lg:w-[386px] mb-4 lg:mb-0"
+            />
+            <button className="bg-[#F26522] text-white font-bold px-6 py-4 rounded-md lg:ml-2 font-Oxanium">
+              Subscribe
+            </button>
+          </div>
         </div>
       </div>
+
       {/* ---------- Footer second layer ---------- */}
       <div className="bg-footerSecondary mt-20">
-        <div className="max-w-max-width mx-auto py-16 lg:flex max-lg:ml-10 items-center">
+        <div className="max-w-max-width mx-auto py-16 xl:flex max-lg:ml-10 items-center">
           <div className="w-1/2">
             <div>
               <img src={logo} alt="logo" />
@@ -41,7 +45,7 @@ const Footer = () => {
               company in your area.
             </p>
           </div>
-          <div className="w-1/2 lg:flex max-lg:space-y-10 max-lg:mt-10 justify-between">
+          <div className="w-1/2 lg:flex max-xl:space-y-10 max-lg:mt-10 justify-between">
             <div>
               <h3 className="text-white font-Oxanium">Other links</h3>
               <ul className="text-[#9291A1] mt-5 space-y-2 font-Oxanium">
@@ -71,7 +75,7 @@ const Footer = () => {
       </div>
       {/* ---------- Footer third layer ---------- */}
       <div>
-        <div className="max-w-max-width mx-auto flex justify-between text-[#DBDBE9] mt-10 font-Raleway">
+        <div className="max-w-max-width mx-auto lg:flex max-lg:ml-5 max-lg:space-y-5 justify-between text-[#DBDBE9] mt-10 font-Raleway">
           <p>All rights Reserved © Your Company, 2021</p>
           <p>
             Made with heart by{" "}
